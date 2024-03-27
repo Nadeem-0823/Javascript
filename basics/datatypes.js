53cdd849f0526bc00685455ef4e3bfb2388572f2
Nadeem Abbas <164657986+Nadeem-0823@users.noreply.github.com>
2024-03-27 19:04:33 +0000

    datatypes file added

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let names ="Nadeem Abbas"
+
+// alert(names) // This is not allowed
+console.log(names)
+
+// DAtatypes are as under
+/* let isLoggedIn = true/false 
+let state
+let number
+let bigint
+let string
+let boolean = true/false
+let sums = null
+undefined
+symbol (use for uniqueness)
+object
+typeof
+*/
+
+console.log(typeof names)
+console.log(typeof null)
+console.log(typeof undefined)
