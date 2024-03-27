c5b85c49a0dbc61c588b47be817616ee203dfb82
Nadeem Abbas <164657986+Nadeem-0823@users.noreply.github.com>
2024-03-27 18:38:29 +0000

    Variablesjs file added

---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountName ="Nadeem Abbas"
+var accountEmail = "nadeem@gmail.com"
+accountCity = "Sargodha"
+let accountState
+
+console.table([accountId, accountName, accountEmail, accountCity, accountState])
