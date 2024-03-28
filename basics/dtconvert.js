e9731464e03ceee4322715581733b949ba001a1e
Nadeem Abbas <164657986+Nadeem-0823@users.noreply.github.com>
2024-03-28 17:31:06 +0000

    datatype conversion P1

---
+++ basics/dtconvert.js
@@ -0,0 +1,63 @@
+// let a = "56"
+// console.log(typeof a)
+// console.log(typeof(a))
+
+// let b = "70"
+// let isNumber = Number(b)
+// console.log(typeof isNumber)
+// console.log(isNumber)
+
+// let c = "56abc"
+// let isNumber2 = Number(c)
+// console.log(typeof isNumber2)
+// console.log(isNumber2)
+
+// let d1 = true
+// console.log(typeof d1)
+// console.log(d1)
+
+// let d2 = false
+// console.log(typeof d2)
+// console.log(d2)
+
+// let d3 = null
+// console.log(typeof d3)
+// console.log(d3)
+
+// let d4 = undefined
+// console.log(typeof d4)
+// console.log(d4)
+
+// let score = Number(d1)
+// console.log(score)
+
+
+// let n1 = null
+// let score2 = Number(null)
+// console.log(score2)
+
+// let n2 = undefined
+// let score3 = Number(undefined)
+// console.log(score3)
+
+// let isLoggedIn = 1
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn)
+
+// let isLoggedIn2 = 0
+// let booleanLoggedIn2 = Boolean(isLoggedIn2)
+// console.log(booleanLoggedIn2)
+
+// let isLoggedIn3 = ""
+// let booleanLoggedIn3 = Boolean(isLoggedIn3)
+// console.log(booleanLoggedIn3)
+
+// let isLoggedIn4 = "Nadeem"
+// let booleanLoggedIn4 = Boolean(isLoggedIn4)
+// console.log(booleanLoggedIn4)
+
+
+// let n3 = 804
+// let stringNumber = String(n3)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
