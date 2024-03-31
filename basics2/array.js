8f91be8ff3dcdb6e975ffa3196b4e0b60d942d15
Nadeem Abbas <164657986+Nadeem-0823@users.noreply.github.com>
2024-03-31 16:59:33 +0000

    array file added

---
+++ basics2/array.js
@@ -0,0 +1,43 @@
+// let myArr = [11,12,13,14,15]
+// console.log(myArr)
+// console.log(myArr[3])
+// console.log(typeof myArr)
+
+// let myArr2 = ["Apple", true, "Banana"]
+// myArr2[6] = "Grapes"
+// console.log(myArr2)
+// console.log(Object.keys(myArr2))
+// console.log(myArr2[4])
+// console.log(myArr2[2])
+// console.log(myArr2.length)
+
+// let myArr3 = new Array("Copy", 15, null, true, "Book")
+// console.log(myArr3)
+
+// let myArr4 = ["RAM", "ROM", "CPU", 1024, "MOUSE", "KEYBOARD"]
+// console.log(myArr4)
+// myArr4.push("Graphic Card") // i element add at end
+// console.log(myArr4)
+// myArr4.pop() // 1 element remove from last
+// console.log(myArr4)
+// myArr4.unshift("Monitor") // 1 element at start
+// console.log(myArr4) 
+// myArr4.shift() // 1 element from start
+// console.log(myArr4)
+// myArr4.shift()
+// console.log(myArr4)
+// console.log(myArr4.includes("ROM"))
+// console.log(myArr4.includes(1000))
+// console.log(myArr4.indexOf("MOUSE"))
+// console.log(myArr4.indexOf(1000))
+
+let myArr5 = ["Nadeem", "Qayyum", "Sarfraz", "Adeel", "Tahir"]
+let myArr6 = myArr5.join() // join() method convert Array into String
+console.log(myArr5)
+console.log(typeof myArr5)
+console.log(myArr6)
+console.log(typeof myArr6)
+
+
+
+
