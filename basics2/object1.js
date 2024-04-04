f0975856b589ff9e7c844803f7e52634c2633073
Nadeem Abbas <164657986+Nadeem-0823@users.noreply.github.com>
2024-04-04 18:21:08 +0000

    object1 commit 2

--- basics2/object1.js
+++ basics2/object1.js
@@ -3,8 +3,8 @@
 
 const mySym = Symbol("key1") // Symbol is decleared
 const JsUser = {
-    [mySym]: "myKey1", // Here symbol is defined, without [] its type is string, it is wrong
-    name: "Nadeem", // it process like a string
+    [mySym]: "myKey1", // Here symbol is defined, without [] its type is string which is wrong, must use []
+    name: "Nadeem", // it process like a string i.e. "name": "Nadeem"
     age: 25,
     location: "Sargodha",
     email: "nadeem@gmail.com",
@@ -14,14 +14,25 @@ const JsUser = {
     LastLoginDays: ["Monday", "Tuesday"]
 }
 
-JsUser.name = "Zeeshan"
+// JsUser.name = "Zeeshan"
 // Object.freeze(JsUser) // freeze the object values so cannot be change
-JsUser.age = 50 // overwrite age
-console.log(JsUser.email)
-console.log(JsUser.name)
-console.log(JsUser["position"])
-console.log(JsUser["full name"])
-console.log(JsUser[mySym]) // Here Symbol is print
-console.log(JsUser.age);
+// JsUser.age = 50 // overwrite age
+// console.log(JsUser.email)
+// console.log(JsUser.name)
+// console.log(JsUser["position"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym]) // Here Symbol is print
+// console.log(JsUser.age);
+// console.log(JsUser)
 
+// Now check a function
+JsUser.greeting = function(){
+    console.log("Hello JS User")
+}
+console.log(JsUser.greeting)
+console.log(JsUser.greeting())
 
+JsUser.greeting2 = function(){
+    console.log(`Hello Js User, ${this.name}`)
+}
+console.log(JsUser.greeting2());
