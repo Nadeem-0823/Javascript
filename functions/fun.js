9bd9697ba3660aebbd6ae918b70728a2e7cae9eb
Nadeem Abbas <164657986+Nadeem-0823@users.noreply.github.com>
2024-04-06 13:04:25 +0000

    fun with rest operator

--- functions/fun.js
+++ functions/fun.js
@@ -10,6 +10,8 @@ addNum(50, " Nadeem");
 const result = addNum(1500, 2400);
 // console.log("Result = ", result); // Here check the return type of function
 
+// **************************************************************************************
+
 function addNum2(n1, n2){
     // return n1 + n2;
 let results = n1 + n2;
@@ -19,6 +21,8 @@ let results = n1 + n2;
 
 const results = addNum2(35000, 45000);
 // console.log("Results = ", results);
+
+// ************************************************************************************************************
 
 // Ways to pass values in function
 
@@ -33,5 +37,37 @@ function loginUser(userName){
     return `${userName} logged in`;
 }
 
-console.log(loginUser("Nadeem"));
+// console.log(loginUser("Nadeem"));
 // console.log(loginUser()); 
+
+// *************************************************************************************
+
+// funtions with unknown Number of Items
+function showNum(...num){ // Rest OPerator/ Spread Operator
+// console.log(num);
+}
+showNum(10,20,30,40,50);
+// Another Example, functions with unknown Number of Items
+function showNum2(val1, val2, ...num2){ 
+    // console.log(num2);
+    }
+    showNum2(10,20,30,40,50);
+
+// ***************************************************************************************************
+
+// function with objects
+
+const user = {
+    userName: "Nadeem",
+    id: 399
+}
+function handleObject(anyObject){
+console.log(`UserName is ${anyObject.userName} and id is ${anyObject.id}`);
+}
+// console.log(handleObject(user));
+
+// another example, function with objects
+function handleObject2(anyObject2){
+console.log(`Company Name is ${anyObject2.company} and Price is ${anyObject2.price}`);
+}
+console.log(handleObject2({company: "Care", price: 3000}));
