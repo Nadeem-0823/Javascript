e8e6a1e82ba62f21051b75edf52be18e9006a3f7
Nadeem Abbas <164657986+Nadeem-0823@users.noreply.github.com>
2024-04-06 10:29:18 +0000

    function file first commit

---
+++ functions/fun.js
@@ -0,0 +1,35 @@
+function addNum(num1, num2){  // parameters
+// console.log(num1 + num2);
+}
+addNum(50, 100);  // arguments
+addNum("Hello ", false);
+addNum("Hello ", null);
+addNum(50, " 100");
+addNum(50, " Nadeem");
+
+const result = addNum(1500, 2400);
+// console.log("Result = ", result); // Here check the return type of function
+
+function addNum2(n1, n2){
+    // return n1 + n2;
+let results = n1 + n2;
+    return results;
+    // console.log("This message never displayed because after return statement no statement executed");
+}
+
+const results = addNum2(35000, 45000);
+// console.log("Results = ", results);
+
+// Ways to pass values in function
+
+function loginUser(userName = "Guest"){ // Guest executed when no username is passed
+    // check that username entered or not
+    // if(userName === undefined){
+       if(!userName){  // empty string " " and undefined both are false values, we use also this code
+        console.log("Please enter a valid username");
+        return; // because of this next return statement not executed
+    }
+    return `${userName} logged in`;
+}
+// console.log(loginUser("Nadeem"));
+console.log(loginUser()); 
