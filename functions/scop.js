a2ae9a1c3f949f738f03cb8f4e737d225ff00fac
Nadeem Abbas <164657986+Nadeem-0823@users.noreply.github.com>
2024-04-07 18:24:14 +0000

    scope 2nd commit

--- functions/scop.js
+++ functions/scop.js
@@ -8,5 +8,43 @@ if (true) {
 }
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
 // **********************************************************************
+// Nested Scope
+// Using function
+function one(){
+    const userName = "Nadeem Abbas";
+    function two(){
+        const website = "www.techcity.com";
+        console.log(userName);
+    }
+    // console.log(website);
+    two();
+}
+// one();
+
+// Using if else
+if (true) {
+    const user = "Sarfraz";
+    if (user === "Sarfraz") {
+        const myWeb = " www.techcity.com";
+        // console.log(user + myWeb);
+    }
+    // console.log(myWeb);
+}
+// console.log(user);
+
+// *********************************************
+
+// Methods to declare functions OR function Hoisting
+console.log(addOne(5)); // function call before initilization, is working
+function addOne(num){
+    return num + 1;
+}
+// console.log(addOne(5)); // function call after initilization, is working
+// ***********************
+// console.log(addTwo(10)); // function call before initilization, not working
+const addTwo = function(num2){
+    return num2 + 2;
+}
+console.log(addTwo(10)); // function call after initilization, is working
