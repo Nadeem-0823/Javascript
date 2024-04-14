b6a7ef0bca6bd433a51249d6db93b12bc8945ce6
Nadeem Abbas <164657986+Nadeem-0823@users.noreply.github.com>
2024-04-14 13:38:41 +0000

    while loop commit

--- loop/while.js
+++ loop/while.js
@@ -1,5 +1,13 @@
-let ele = 1;
-while (ele <=5) {
-    console.log(ele);
-    ele++;
-}
+// let ele = 2;
+// while (ele <=10) {
+//     console.log(`Value of Loop = ${ele}`);
+//     ele = ele + 2;
+// }
+
+// While Loop with Array
+// let myArr = [10, 20, 30 , 40 ,50];
+// let m = 0;
+// while (m < myArr.length) {
+//     console.log(`Value of Array ${myArr[m]}`);
+//     m++;
+// }
