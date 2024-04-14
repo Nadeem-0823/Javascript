07e46e6eb10afbd09ffd9e642564695583336c4a
Nadeem Abbas <164657986+Nadeem-0823@users.noreply.github.com>
2024-04-14 13:44:58 +0000

    dowhile commit

---
+++ loop/dowhile.js
@@ -0,0 +1,12 @@
+// let score = 0;
+// do {
+//     console.log(`Score = ${score}`);
+//     score++;
+// } while (score <= 6);
+
+// Another case where initial value is greater than condition valuse
+let score1 = 12;
+do {
+    console.log(`Score1 = ${score1}`);
+    score1++;
+} while (score1<=5);
